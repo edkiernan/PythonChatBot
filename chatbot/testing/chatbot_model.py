ddce9feaa30c92bf7966e0c9f871b64015193002
Edward K <edki2977@colorado.edu>
2023-08-13 18:43:21 -0600

    Reworking file locations

---
+++ chatbot/testing/chatbot_model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+
+
+encoder_inputs = tf.keras.layers.Input(shape=( None , ))
+encoder_embedding = tf.keras.layers.Embedding( num_tokens, 200 , mask_zero=True) (encoder_inputs)
+encoder_outputs , state_h , state_c = tf.keras.layers.LSTM( 200 , return_state=True )( encoder_embedding )
+encoder_states = [ state_h , state_c ]
+
+decoder_inputs = tf.keras.layers.Input(shape=( None ,  ))
+decoder_embedding = tf.keras.layers.Embedding( num_tokens, 200 , mask_zero=True) (decoder_inputs)
+decoder_lstm = tf.keras.layers.LSTM( 200 , return_state=True , return_sequences=True )
+decoder_outputs , _ , _ = decoder_lstm ( decoder_embedding , initial_state=encoder_states )
+decoder_dense = tf.keras.layers.Dense( num_tokens , activation=tf.keras.activations.softmax ) 
+output = decoder_dense ( decoder_outputs )
+
+model = tf.keras.models.Model([encoder_inputs, decoder_inputs], output )
+model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='categorical_crossentropy')
+
+model.summary()
