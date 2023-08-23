f3041661ae31f62f6e8eeac6f36a34b13320d5b7
Edward K <edki2977@colorado.edu>
2023-08-23 17:41:45 -0600

    Work on training data

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 #create chatbot
-englishBot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ChatterBotCorpusTrainer(englishBot)
-trainer.train("chatterbot.corpus.english") #train the chatter bot for english
+my_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(my_bot)
+trainer.train(
+   "./training"
+) #train the chatter bot on training data
 
 #define app routes
 @app.route("/")
@@ -18,7 +20,7 @@ def index():
 #function for the bot response
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(englishBot.get_response(userText))
+    return str(my_bot.get_response(userText))
 
 if __name__ == "__main__":
     app.run()
